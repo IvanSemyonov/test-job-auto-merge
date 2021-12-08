6dddf6d7a2c7f33c225fe9693dfcd77e0390732d
IvanSemyonov <semyonov.o2001@gmail.com>
2021-12-08 11:39:30 +0300

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,17 +7,6 @@ public class Main {
     }
 
     private static void method() {
-        System.out.println("cxsfcx");
-    }
-
-    private static void newMethod(int blabla) {
-        System.out.println(blabla);
-    }
-
-    private static void method(int f) {
         System.out.println("ggg");
-    }
-
-    private void newMethod() {
     }
 }
