4817ba0e044098b3bbbac35c3c711c180789f367
IvanSemyonov <semyonov.o2001@gmail.com>
2021-12-24 12:06:29 +0300

    ...

--- .github/actions/webex/entrypoint.sh
+++ .github/actions/webex/entrypoint.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
 
-_wxt_checks() {
-  if [ -z "${TOKEN}" ]; then
-    _error "TOKEN is not set."
-  fi
-
-  if [ -z "${ROOMID}" ]; then
-    _error "ROOMID is not set."
-  fi
-
-  if [ -z "${MESSAGE}" ]; then
-    _error "MESSAGE is not set."
-  fi
-}
-
 _wxt_message() {
-  _wxt_checks
 
   URL="https://api.ciscospark.com/v1/messages/"
   status_code=$(curl --write-out %{http_code} --silent --output /dev/null \
@@ -25,7 +10,7 @@ _wxt_message() {
     --form "markdown=${MESSAGE}" \
     ${URL} )
 
-  echo "Site status changed to $status_code"
+  echo "Status: $status_code"
 
   if [[ "$status_code" -ne 200 ]] ; then
     exit 1
