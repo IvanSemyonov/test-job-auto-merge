ddc29d5ea93e3cbbb08cb027d963dc3eebdfa6db
IvanSemyonov <semyonov.o2001@gmail.com>
2021-12-27 08:58:37 +0300

    ...

--- .github/actions/webex/entrypoint.sh
+++ .github/actions/webex/entrypoint.sh
@@ -3,17 +3,18 @@
 _wxt_message() {
 
   URL="https://api.ciscospark.com/v1/messages/"
-  http_response = $(curl -s -o response.txt -w \
+  http_response=$(curl -i -o - --silent \
     -X POST \
     -H "Authorization:Bearer ${TOKEN}" \
     --form "roomId=${ROOMID}" \
     --form "markdown=${MESSAGE}" \
-    ${URL} \
-    --data-binary $1 )
+    ${URL} )
 
-  if [[ $response != 200 ]] ; then
-    echo "Error: "
-    cat response.txt
+    http_status=$(echo "http_response" | grep HTTP |  awk '{print $2}')
+
+  if [[ "$http_status" -ne 200 ]] ; then
+    response_body=$(echo "$request_cmd" | grep body)
+    echo "Error: $(response_body)"
     exit 1
   else
     exit 0
