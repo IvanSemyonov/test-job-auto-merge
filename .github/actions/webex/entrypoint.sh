12a61e552c5b04410227af65d6c69db4b86cf82c
IvanSemyonov <semyonov.o2001@gmail.com>
2021-12-27 08:44:16 +0300

    ...

--- .github/actions/webex/entrypoint.sh
+++ .github/actions/webex/entrypoint.sh
@@ -3,17 +3,22 @@
 _wxt_message() {
 
   URL="https://api.ciscospark.com/v1/messages/"
-  status_code=$(curl --write-out %{http_code} --silent --output /dev/null \
+  response = $(curl -s -w "%{http_code}" \
     -X POST \
     -H "Authorization:Bearer ${TOKEN}" \
     --form "roomId=${ROOMID}" \
     --form "markdown=${MESSAGE}" \
-    ${URL} )
+    ${URL} \
+    --data-binary $1 )
+
+  body=${res::-3}
+  status_code = $(printf "%s" "response" | tail -c 3)
 
   echo "POST $URL"
   echo "Status: $status_code"
 
   if [[ "$status_code" -ne 200 ]] ; then
+    echo body
     exit 1
   else
     exit 0
