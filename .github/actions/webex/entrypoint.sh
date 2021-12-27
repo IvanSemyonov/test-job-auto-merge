56db06303a5427540cdbfca3266fb1de13509b36
IvanSemyonov <semyonov.o2001@gmail.com>
2021-12-27 09:01:40 +0300

    ...

--- .github/actions/webex/entrypoint.sh
+++ .github/actions/webex/entrypoint.sh
@@ -12,9 +12,12 @@ _wxt_message() {
 
     http_status=$(echo "http_response" | grep HTTP |  awk '{print $2}')
 
+    echo "RESPONSE:"
+    echo $http_response
+
   if [[ "$http_status" -ne 200 ]] ; then
     response_body=$(echo "$request_cmd" | grep body)
-    echo "Error: $(response_body)"
+    echo "Error: $response_body"
     exit 1
   else
     exit 0
